a46d0635a83e5f5e7b30d4ce7a660607dcdaca8d
Zainfarhan2000 <zainthegamer12@gmail.com>
2023-06-28 20:44:40 +0500

    Main Program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,123 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.BackgroundColor = ConsoleColor.White;
+Console.ForegroundColor = ConsoleColor.Black;
+Console.Clear();
+
+int repeat = 1;
+int choice;
+int bookIndex;
+Book[] Library = new Book[999];
+
+for (int i = 0; i < Library.Length; i++)
+{
+    Library[i] = new Book();
+}
+
+do
+{
+    Console.WriteLine("Press 1 to view Book | Press 2 to Set Book");
+    choice = Convert.ToInt32(Console.ReadLine());
+
+
+
+    if (choice == 1)
+    {
+        Console.Write("Enter Book Index: ");
+        bookIndex = Convert.ToInt32(Console.ReadLine());
+        bookIndex--;
+
+        See(bookIndex);
+    }
+    else
+    {
+        Console.Write("Enter Book Index: ");
+        bookIndex = Convert.ToInt32(Console.ReadLine());
+        bookIndex--;
+
+        Change(bookIndex);
+
+    }
+
+
+} while (repeat == 1);
+
+
+
+
+void See(int index)
+{
+    Console.WriteLine("Author: " + Library[index].author);
+    Console.WriteLine("Publisher: " + Library[index].publisher);
+    Console.WriteLine("Pages: " + Library[index].pages);
+    Console.WriteLine("IMBC Number: " + Library[index].imbc);
+
+}
+
+Book Change(int index)
+{
+    string? tempString;
+    int tempInt = 0;
+
+    Console.Write("Author: ");
+    tempString = Console.ReadLine();
+
+    if (tempString != "")
+    {
+        Library[index].author = tempString;
+    }
+
+    Console.Write("Publisher: ");
+    tempString = Console.ReadLine();
+
+    if (tempString != "")
+    {
+        Library[index].publisher = tempString;
+    }
+
+    Console.Write("Pages: ");
+    tempString = Console.ReadLine();
+
+    try 
+    {
+        tempInt = Convert.ToInt32(tempString);
+    }
+    catch
+    {
+
+    }
+
+    if (tempString != "")
+    {
+        Library[index].pages = tempInt;
+    }
+
+    Console.Write("IMBC Number: ");
+    tempString = Console.ReadLine();
+
+    try 
+    {
+        tempInt = Convert.ToInt32(tempString);
+    }
+    catch
+    {
+
+    }
+
+    if (tempString != "")
+    {
+        Library[index].imbc = tempInt;
+    }
+
+    return Library[index];
+
+
+}
+
+class Book
+{
+    public string? author = "<EMPTY>";
+    public string? publisher = "<EMPTY>";
+    public int pages;
+    public int imbc;
+
+}
